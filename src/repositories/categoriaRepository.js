4cddfb184f328164577125a03f14dfc159947b6f
Florisvaldo Junior <florisvaldoantonio2@gmail.com>
2026-06-03 10:37:20 -0300

    correção no retorno das rotas

--- src/repositories/categoriaRepository.js
+++ src/repositories/categoriaRepository.js
@@ -4,23 +4,23 @@ const categoriaRepository = {
     criar: async (categoria) => {
         const sql = 'INSERT INTO categorias (Nome, Descricao) VALUES (?, ?);';
         const values = [categoria.nome, categoria.descricao];
-        const rows = await connection.execute(sql, values);
+        const {rows} = await connection.execute(sql, values);
         return rows;
     },
     editar: async (categoria) => {
         const sql = 'UPDATE categorias SET Nome = ?, Descricao = ? WHERE id = ?;';
         const values = [categoria.nome, categoria.descricao, categoria.id];
-        const rows = await connection.execute(sql, values);
+        const {rows} = await connection.execute(sql, values);
         return rows;
     },
     deletar: async (id) => {
         const sql = 'DELETE FROM categorias WHERE id = ?;';
-        const rows = await connection.execute(sql, [id]);
+        const {rows} = await connection.execute(sql, [id]);
         return rows;
     },
     selecionar: async () => {
         const sql = 'SELECT * FROM categorias;';
-        const rows = await connection.execute(sql);
+        const {rows} = await connection.execute(sql);
         return rows;
     }
 }
--- src/repositories/produtoRepository.js
+++ src/repositories/produtoRepository.js
@@ -4,23 +4,23 @@ const produtoRepository = {
     criar: async (produto) => {
         const sql = 'INSERT INTO produtos (nome, valor, id_categoria) VALUES (?, ?, ?);';
         const values = [produto.nome, produto.valor, produto.idCategoria];
-        const rows = await connection.execute(sql, values);
+        const {rows} = await connection.execute(sql, values);
         return rows;
     },
     editar: async (produto) => {
         const sql = 'UPDATE produtos SET nome = ?, valor = ?, id_categoria = ? WHERE id = ?;';
         const values = [produto.nome, produto.valor, produto.idCategoria, produto.id];
-        const rows = await connection.execute(sql, values);
+        const {rows} = await connection.execute(sql, values);
         return rows;
     },
     deletar: async (id) => {
         const sql = 'DELETE FROM produtos WHERE id = ?;';
-        const rows = await connection.execute(sql, [id]);
+        const {rows} = await connection.execute(sql, [id]);
         return rows;
     },
     selecionar: async () => {
         const sql = 'SELECT * FROM produtos;';
-        const rows = await connection.execute(sql);
+        const {rows} = await connection.execute(sql);
         return rows;
     }
 }
