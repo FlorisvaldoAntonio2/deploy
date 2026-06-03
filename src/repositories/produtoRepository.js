767d4dcaee17f39977a63599566121eb642816d3
Florisvaldo Junior <florisvaldoantonio2@gmail.com>
2026-06-03 10:48:16 -0300

    correção no retorno das rotas

--- src/repositories/produtoRepository.js
+++ src/repositories/produtoRepository.js
@@ -4,24 +4,24 @@ const produtoRepository = {
     criar: async (produto) => {
         const sql = 'INSERT INTO produtos (nome, valor, id_categoria) VALUES (?, ?, ?);';
         const values = [produto.nome, produto.valor, produto.idCategoria];
-        const {rows} = await connection.execute(sql, values);
-        return rows;
+        const rows = await connection.execute(sql, values);
+        return rows[0];
     },
     editar: async (produto) => {
         const sql = 'UPDATE produtos SET nome = ?, valor = ?, id_categoria = ? WHERE id = ?;';
         const values = [produto.nome, produto.valor, produto.idCategoria, produto.id];
-        const {rows} = await connection.execute(sql, values);
-        return rows;
+        const rows = await connection.execute(sql, values);
+        return rows[0];
     },
     deletar: async (id) => {
         const sql = 'DELETE FROM produtos WHERE id = ?;';
-        const {rows} = await connection.execute(sql, [id]);
-        return rows;
+        const rows = await connection.execute(sql, [id]);
+        return rows[0];
     },
     selecionar: async () => {
         const sql = 'SELECT * FROM produtos;';
-        const {rows} = await connection.execute(sql);
-        return rows;
+        const rows = await connection.execute(sql);
+        return rows[0];
     }
 }
 
